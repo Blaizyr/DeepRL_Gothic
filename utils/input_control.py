0f04dd319b0373908e2357c0887416994c721408
p_woźniak <blaizyr@gmail.com>
2025-08-25 23:47:05 +0200

    refactor(input): encapsulate action execution in InputController class
    - Replaced standalone send_action with InputController class
    - Supports per-action timing with randomness for more realistic behavior
    - Handles complex actions: jump, macro, side_step, hold, bow_shooting
    - Provides consistent timing_info output for environment feedback
    - Removed old commented-out procedural code

--- utils/input_control.py
+++ utils/input_control.py
@@ -1,70 +1,91 @@
 # utils/input_control.py
+import random
 import time
 import pydirectinput as pdi
 
 pdi.PAUSE = 0.01
 
-def send_action(action_id: int, hold_ms: int = 60):
-    action_map = {
-        0: None,
-        1: "up",
-        2: "down",
-        3: "left",
-        4: "right",
-        5: "ctrl",
-        6: "alt",
-        7: "1",
-        8: "2",
-        9: "3",
-        10: ('macro', 'ctrl', 'right', 100),
-        11: ('macro', 'ctrl', 'left', 100),
-        12: ('macro', 'ctrl', 'up', 100),
-        13: ('macro', 'ctrl', 'down', 100),
-        14: ('macro', 'alt', 'up', 100),
-        15: ('macro', 'up', 'alt', 300),
-        16: ('macro', 'up', 'num 0', 300),
-        # 16: ('macro', 'down', 'num 0', 300),
-        # 16: ('macro', 'ctrl', 'alt', 'left', 100),
-        # 15: ('macro', 'ctrl', 'alt', 'right', 100),
-    }
 
-    action = action_map.get(action_id)
-    timing_info = {}
+class InputController:
+    def __init__(self, action_map, default_hold_ms: int = 60):
+        self.default_hold_ms = default_hold_ms
+        self.action_map = action_map
 
-    if action is None:
-        time.sleep(hold_ms / 1000.0)
-        timing_info['type'] = 'idle'
+    def execute_action(self, action_id: int, hold_ms: int = None):
+        hold_ms = hold_ms or self.default_hold_ms
+        action = self.action_map.get(action_id)
+        timing_info = {}
+        start = time.time()
+
+        if action is None:
+            time.sleep(hold_ms / 1000.0)
+            return {'type': 'idle'}
+
+        if isinstance(action, tuple):
+            action_type = action[0]
+
+            if action_type == 'jump':
+                a_type, k1, k2, a_time_ms = action
+                pdi.keyDown(k1)
+                time.sleep(a_time_ms)
+                pdi.keyDown(k2)
+                time.sleep(0.02)
+                pdi.keyUp(k2)
+                time.sleep(max(0, a_time_ms))
+                pdi.keyUp(k1)
+                timing_info.update({'type': 'jump', 'keys': (k1, k2)})
+
+            elif action_type == 'macro':
+                a_type, mod, key, a_time_ms = action
+                pdi.keyDown(mod)
+                time.sleep(0.02)
+                pdi.keyDown(key)
+                time.sleep(0.02)
+                pdi.keyUp(key)
+                time.sleep(random.randrange(0, a_time_ms / 1000.0 - 0.04))
+                pdi.keyUp(mod)
+                timing_info.update({'type': 'macro', 'keys': (mod, key)})
+
+            elif action_type == 'side_step':
+                a_type, k1, k2, a_time_ms = action
+                pdi.keyDown(k1)
+                time.sleep(0.02)
+                pdi.keyDown(k2)
+                time.sleep(a_time_ms / 1000.0)
+                pdi.keyUp(k2)
+                time.sleep(0.02)
+                pdi.keyUp(k1)
+                timing_info.update({'type': 'side_step', 'keys': (k1, k2)})
+
+            elif action_type == 'hold':
+                a_type, key, a_time_ms = action
+                actual_hold = random.randrange(hold_ms, a_time_ms) / 1000.0
+                pdi.keyDown(key)
+                time.sleep(actual_hold)
+                pdi.keyUp(key)
+                timing_info.update({'type': 'hold', 'key': key})
+
+            elif action_type == 'bow_shooting':
+                a_type, aim, shoot, a_time_ms = action
+                actual_hold = random.randrange(1, a_time_ms / 1000.0)
+                pdi.keyDown(aim)
+                time.sleep(0.02)
+                pdi.keyDown(shoot)
+                time.sleep(actual_hold)
+                pdi.keyUp(shoot)
+                time.sleep(0.02)
+                pdi.keyUp(aim)
+                timing_info.update({'type': 'bow_shooting', 'keys': (aim, shoot)})
+
+            else:
+                timing_info.update({'type': 'unknown', 'action': action})
+
+        else:
+            key = action
+            pdi.keyDown(key)
+            time.sleep(hold_ms / 1000.0)
+            pdi.keyUp(key)
+            timing_info.update({'type': 'basic', 'key': key})
+
+        timing_info['duration'] = time.time() - start
         return timing_info
-
-    if action[0] != 'macro':
-        key = action
-        start = time.time()
-        pdi.keyDown(key)
-        time.sleep(3 * hold_ms / 1000.0)
-        pdi.keyUp(key)
-        end = time.time()
-        timing_info['duration'] = end - start
-        timing_info['type'] = 'basic'
-
-    else:
-        _, mod_key, key, _ = action
-        start = time.time()
-
-        pdi.keyDown(mod_key)
-        time.sleep(0.02)
-        pdi.keyDown(key)
-        inner_delay = 0.02
-        time.sleep(0.02)
-        pdi.keyUp(key)
-        time.sleep((hold_ms / 1000.0) - 0.04)
-        pdi.keyUp(mod_key)
-
-        total_time = time.time() - start
-        timing_info.update({
-            'type': 'macro',
-            'action_to_arrow_delay': inner_delay,
-            'total_duration': total_time,
-            'requested_hold_ms': hold_ms
-        })
-
-    return timing_info
